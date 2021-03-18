854c995242cf12b763d0256ce6cbc05edf989047
Bob.C <bob.cocksedge@gmail.com>
2021-03-18 19:06:28 +0000

    Create suffix A files and attempt to reproduce functions

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # ns_w3Funcs
 Revise functions through w3 schools
-Tutorials to work through:
-JS Functions
-1 Function Definitions
+"Tutorials to work through: Definitions  Parameters Invocation Call Apply Closures"
+JS Function 
+1 Function Definitions: to run these examples simoultaneaously they must have different function names and variable names. 
 2 Function Parameters
 3 Function Invocation
 4 Function Call
 5 Function Apply
 6 Function Closures
+"Create suffix A files and attempt to reproduce functions"
--- jscript.js
+++ jscript.js
@@ -1,1 +1,36 @@
 //Function Definitions
+//Function Declaration
+const x = myFunction(4, 3);   //var let but not const!
+document.getElementById("demo1").innerHTML = x;
+
+function myFunction(a, b) {
+  return a * b;
+ } //a function declaration is not an executable statement, it is not common to end it with a semicolon.
+ 
+//  //Function Expression anonymous functions and invoked (called) using the variable name.
+ const y= function (a, b) {return a * b};            
+document.getElementById("demo2").innerHTML = y;
+// //Function Expression
+const z = function (a, b) {return a * b};
+document.getElementById("demo3").innerHTML = z(4, 3);
+
+//The Function() Constructor
+const aFunction = new Function("a", "b", "return a * b");
+document.getElementById("demo4").innerHTML = aFunction(4, 3);
+//The Function() Constructor but without new or capital F
+const bFunction = function (a, b) {return a * b}
+document.getElementById("demo5").innerHTML = bFunction(4, 3);
+
+//Self-Invoking Functions
+(function () {
+    document.getElementById("demo6").innerHTML = "Hello! I called myself";
+})();
+
+//Functions Can Be Used as Values
+function cFunction(a, b) {
+    return a * b;
+  }
+  const c = cFunction(4, 3);
+  document.getElementById("demo7").innerHTML = c;
+  console.log(c);
+
